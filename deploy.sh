4a597fd9b8154946d5e1a53f6af77cadf34c05d2
Leon Yu <leon@leonyu.net>
2018-12-28 03:23:31 +0800

    use mp3

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-aws s3 cp --acl public-read ./index.html s3://www.jimboche.com/
-aws s3 cp --acl public-read ./jimboche.jpeg s3://www.jimboche.com/
-aws s3 cp --acl public-read ./ChepteCheptePerungo.ogg s3://www.jimboche.com/
+
+aws s3 cp --acl public-read --exclude '.*' --exclude '*.sh' --recursive . s3://www.jimboche.com/

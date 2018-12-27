72cc2d335f562540f2e3ae3004d40f1235c65dad
Leon Yu <leon@leonyu.net>
2018-12-28 01:41:33 +0800

    add audio

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 aws s3 cp --acl public-read ./index.html s3://www.jimboche.com/
 aws s3 cp --acl public-read ./jimboche.jpeg s3://www.jimboche.com/
-
+aws s3 cp --acl public-read ./ChepteCheptePerungo.ogg s3://www.jimboche.com/
